26c6dad96ae600202b11e26757d6b1d6cac52f5d
BlizPerfect <zax_2000@mail.ru>
2021-09-27 21:20:24 +0500

    Реализован базовый функционал для положительного сдвига.
    Реализовано кодирование методом шифра цезаря для положительного шага.

---
+++ AppProg_2/AppProg_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AppProg_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            LinkedList<char> alphabetTestNew = new LinkedList<char>();
+
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alphabetTest = "abc".ToCharArray();
+            Console.WriteLine("Введите предложение для щифрования.");
+            var originalStringInCharArray = Console.ReadLine().ToCharArray();
+
+            Console.Write("Укажите шаг сдвига: ");
+            var step = Convert.ToInt32(Console.ReadLine()) % 26;
+            //25 норм, 26 слом
+            for (var i = 0; i < originalStringInCharArray.Length; i++)
+            {
+                var positionInAlphabet = Array.IndexOf(alphabet, originalStringInCharArray[i]);
+                if (step > 0)
+                {
+                    Console.WriteLine(alphabet[(positionInAlphabet + step) % 26]);
+                }
+
+
+
+            }
+        }
+    }
+}
