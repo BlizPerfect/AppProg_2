d1f0df5ca3a6b39c34bb63afa64994721af02958
BlizPerfect <zax_2000@mail.ru>
2021-09-27 22:53:34 +0500

    Функционал дополнен до требуемого.
    1. Добавлен выбор опции шифровки и расшифровки строки.
    2. Реализована возможность расшифровки строки для положительного и отрицательного сдвига.
    3. Отлов багов.
    4. Рефакторинг кода.

--- AppProg_2/AppProg_2/Program.cs
+++ AppProg_2/AppProg_2/Program.cs
@@ -1,39 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+
 namespace AppProg_2
 {
     class Program
     {
-
-        static void Main(string[] args)
+        public static string Calculate(char[] stringInCharArray, int step, char[] alphabet)
         {
-            LinkedList<char> alphabetTestNew = new LinkedList<char>();
-
-            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            Console.WriteLine("Введите предложение для щифрования.");
-            var originalStringInCharArray = Console.ReadLine().ToCharArray();
-
-            Console.Write("Укажите шаг сдвига: ");
-            var step = Convert.ToInt32(Console.ReadLine()) % 26;
-            for (var i = 0; i < originalStringInCharArray.Length; i++)
+            var result = new StringBuilder();
+            for (var i = 0; i < stringInCharArray.Length; i++)
             {
-                var positionInAlphabet = Array.IndexOf(alphabet, originalStringInCharArray[i]);
-                if (step >= 0)
+                if (Array.IndexOf(alphabet, stringInCharArray[i]) != -1)
                 {
-                    Console.WriteLine(alphabet[(positionInAlphabet + step) % 26]);
-                }
-                else
-                {
-                    if (positionInAlphabet + step >= 0)
+                    var positionInAlphabet = Array.IndexOf(alphabet, stringInCharArray[i]);
+                    var newPosition = 0;
+                    if (step >= 0)
                     {
-                        Console.WriteLine(alphabet[positionInAlphabet + step]);
+                        newPosition = (positionInAlphabet + step) % 26;
                     }
                     else
                     {
-                        Console.WriteLine(alphabet[26 + (positionInAlphabet + step)]);
+                        if (positionInAlphabet + step >= 0)
+                        {
+                            newPosition = positionInAlphabet + step;
+                        }
+                        else
+                        {
+                            newPosition = 26 + (positionInAlphabet + step);
+                        }
                     }
+                    result.Append(alphabet[newPosition]);
+                }
+                else
+                {
+                    result.Append(stringInCharArray[i]);
                 }
             }
+            return result.ToString();
+        }
+        static void Main(string[] args)
+        {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var choise = 0;
+            var step = 0;
+            bool test = true;
+
+            Console.WriteLine("Меню:\n1: Зашифровать строку.\n2: Расшифровать строку");
+            while (test)
+            {
+                try
+                {
+                    choise = Convert.ToInt32(Console.ReadLine());
+                    if (choise < 1 || choise > 2)
+                    {
+                        Console.WriteLine("Такой операции нет, повторите ввод!");
+                    }
+                    else
+                    {
+                        test = false;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте сного ввести число для выбора операции.");
+                }
+            }
+            test = true;
+
+            Console.Write("Введите строку: ");
+            var originalStringInCharArray = Console.ReadLine().ToCharArray();
+            Console.Write("Укажите шаг сдвига: ");
+            while (test)
+            {
+                try
+                {
+                    step = Convert.ToInt32(Console.ReadLine()) % 26;
+                    test = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте сного ввести число для сдвига.");
+                }
+            }
+
+            if (choise.Equals(1))
+            {
+                Console.Write("Зашифрованная строка: ");
+            }
+            else
+            {
+                Console.Write("Расшифрованная строка: ");
+                step *= -1;
+            }
+            var result = Calculate(originalStringInCharArray, step, alphabet);
+            Console.WriteLine(result);
         }
     }
 }
